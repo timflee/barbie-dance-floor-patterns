d5137e340a45f7e7872d220fec99be894a30e65f
timflee <timflee@hotmail.com>
2022-11-07 16:07:55 +1100

    Couple of examples for Joeys

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+function Seq1 () {
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Red), 1)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Blue), 2)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Yellow), 3)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Green), 4)
+    basic.pause(short)
+    tileDisplay.clear()
+    tileDisplay.show()
+}
+function Seq0 () {
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Red), 1)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Blue), 2)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Yellow), 3)
+    basic.pause(short)
+    drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Green), 4)
+    basic.pause(short)
+    tileDisplay.clear()
+    tileDisplay.show()
+}
 function drawRectangles2 (colour: number, rectangleNumber: number, speed: number, dynamicRefresh: boolean) {
     if (rectangleNumber == 1) {
         rectangleToDraw = rectangle1
@@ -13,7 +37,7 @@ function drawRectangles2 (colour: number, rectangleNumber: number, speed: number
     }
     tileDisplay.show()
 }
-function drawRectangles (colour: number, rectangleNumber: number, speed: number, dynamicRefresh: boolean) {
+function drawRectangles (colour: number, rectangleNumber: number) {
     if (rectangleNumber == 1) {
         rectangleToDraw = rectangle1
     } else if (rectangleNumber == 2) {
@@ -25,18 +49,11 @@ function drawRectangles (colour: number, rectangleNumber: number, speed: number,
     }
     for (let value of rectangleToDraw) {
         tileDisplay.setMatrixColor(value[0], value[1], colour)
-        if (dynamicRefresh) {
-            tileDisplay.show()
-        }
-        basic.pause(speed / rectangleToDraw.length)
     }
-    if (!(dynamicRefresh)) {
-        tileDisplay.show()
-    }
+    tileDisplay.show()
 }
 function initVariables () {
     tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
-    tileDisplay.setBrightness(25)
     tileDisplay.clear()
     tileDisplay.show()
     music.playTone(262, music.beat(BeatFraction.Whole))
@@ -115,29 +132,34 @@ function initVariables () {
     ]
 }
 let colour2 = 0
-let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
 let rectangle4: number[][] = []
 let rectangle3: number[][] = []
 let rectangle2: number[][] = []
 let rectangle1: number[][] = []
 let rectangleToDraw: number[][] = []
-let period = 1
-let period2 = 25
+let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
+let short = 0
+let long = 2000
+short = 500
 initVariables()
+tileDisplay.setBrightness(50)
 basic.forever(function () {
     while (true) {
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Red), 1, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 1, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Orange), 2, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 2, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Yellow), 3, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 3, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Green), 4, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 4, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Blue), 3, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 3, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Indigo), 2, 200, false)
-        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 2, 200, false)
-        basic.pause(period)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Red), 1)
+        basic.pause(short)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 1)
+        basic.pause(long)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Blue), 2)
+        basic.pause(short)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 2)
+        basic.pause(long)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Purple), 3)
+        basic.pause(short)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 3)
+        basic.pause(long)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Green), 4)
+        basic.pause(short)
+        drawRectangles(Kitronik_Zip_Tile.colors(ZipLedColors.Black), 4)
+        basic.pause(long)
     }
 })
